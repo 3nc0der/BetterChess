380a3a014a4a6b1e6cc76cd57d8a374ecf1c72e7
3nc0der <finnribbeck@gmail.com>
2020-09-04 13:10:18 +0200

    Refactored the logfile class.

--- logfile.hpp
+++ logfile.hpp
@@ -11,7 +11,7 @@ using namespace std;
 constexpr auto MAX_BUFFER = 1024;
 
 //predefined colors
-typedef enum e_color
+typedef enum class log_color
 {
 	BLACK = 0,
 	WHITE = 1,
@@ -24,58 +24,58 @@ typedef enum e_color
 	DEFAULT = 8
 }LOG_COLOR;
 
-//logfile class, get an instance with the LOGFILE macro
-class c_logfile : public singleton<c_logfile>
+//log file class, get an instance with the LOGFILE macro
+class logfile : public singleton<logfile>
 {
 public:
 
-	c_logfile();
-	~c_logfile();
+	logfile();
+	~logfile();
 
 	//basic writetolog function
-	void mf_writeToLog(const char* text);
+	void writeToLog(const char* text);
 
 	//use one of the predefined colors
-	void mf_writeToLog(const char* text, e_color color);
+	void writeToLog(const char* text, log_color color);
 
 	//if the message is critical it will be visible in the log
-	void mf_writeToLog(const char* text, e_color color, bool critical);
+	void writeToLog(const char* text, log_color color, bool critical);
 
 	//give the wanted color like this "RGB" aka "FF00FF"
-	void mf_writeToLog(const char* text, const char* color);
+	void writeToLog(const char* text, const char* color);
 
 	//if the message is critical it will be visible in the log
-	void mf_writeToLog(const char* text, const char* color, bool critical);
+	void writeToLog(const char* text, const char* color, bool critical);
 
 	//basic writetolog formatted function
-	void mf_writeToLogf(const char* text, ...);
+	void writeToLogf(const char* text, ...);
 
 	//use one of the predefined colors
-	void mf_writeToLogf(e_color color, const char* text, ...);
+	void writeToLogf(log_color color, const char* text, ...);
 
 	//if the message is critical it will be visible in the log
-	void mf_writeToLogf(e_color color, bool critical, const char* text, ...);
+	void writeToLogf(log_color color, bool critical, const char* text, ...);
 
 	//give the color like this "RGB" aka "FF00FF"
-	void mf_writeToLogf(const char* color, const char* text, ...);
+	void writeToLogf(const char* color, const char* text, ...);
 
 	//if the message is critical it will be visible in the log
-	void mf_writeToLogf(const char* color, bool critical, const char* text, ...);
+	void writeToLogf(const char* color, bool critical, const char* text, ...);
 
 private:
 
 	//file variable
-	FILE *mv_logfile;
+	FILE *log;
 
 	//holds current logfile name
-	const char* mv_logName = "logfile.html";
+	const char* logName = "logfile.html";
 
 	//holds the last string that was written to log
-	string mv_lastString = "";
+	string lastString = "";
 
 	//holds amount of last string written
-	int mv_timesLastStringWritten = 0;
+	int timesLastStringWritten = 0;
 
 	//tells wether the write lock is active or not
-	bool mv_writeLock = false;
+	bool writeLock = false;
 };
